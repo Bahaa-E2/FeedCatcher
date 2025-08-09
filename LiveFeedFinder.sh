aef7053e68ddde88530fbcd6feac8c4ce0e8310d
Bahaa-E2 <127204304+Bahaa-E2@users.noreply.github.com>
2025-08-09 19:02:33 +0300

    Update LiveFeedFinder.sh

--- LiveFeedFinder.sh
+++ LiveFeedFinder.sh
@@ -1,38 +1,17 @@
 #!/bin/sh
-# Live FeedFinder Plugin Installer
-# Version: 1.0
-# Author: Bahaa (Linuxcode)
+# Plugin: Live FeedFinder v 1.0
+# By: Bahaa (Linuxcode)
 
 echo "Starting installation..."
-
 sleep 3
 rm -rf /usr/lib/enigma2/python/Plugins/Extensions/Live_FeedFinder
-status_file='/var/lib/opkg/status'
-package_name='enigma2-plugin-extensions-Live_FeedFinder'
 echo "> Installing Beautiful Soup package..."
 opkg install python3-beautifulsoup4
- 
-
-sleep 3
-
 echo "> Downloading Live FeedFinder..."
-wget -q -O /tmp/Live_FeedFinder.tar.gz "https://github.com/yourusername/Live_FeedFinder/raw/refs/heads/main/Live_FeedFinder.tar.gz"
-
-if [ $? -ne 0 ]; then
-    echo "ERROR: Download failed!"
-    exit 1
-fi
-
-echo "> Installing..."
+wget -q -O /tmp/Live_FeedFinder.tar.gz "https://raw.githubusercontent.com/Bahaa-E2/Liver_FeedFinder/refs/heads/main/Live_FeedFinder.tar.gz"
 tar -xzf /tmp/Live_FeedFinder.tar.gz -C /
-if [ $? -ne 0 ]; then
-    echo "ERROR: Extraction failed!"
-    exit 1
-fi
-
 rm -f /tmp/Live_FeedFinder.tar.gz
 sleep 2
-
 sync
 echo "========================================================="
 echo "===                      FINISHED                     ==="
